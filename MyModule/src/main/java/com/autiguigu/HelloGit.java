1b65cac830bd11241b85efe67152e1ee1e50328a
zhangqingming <abcd1234>
2021-04-27 16:13:18 +0800

    提交2

--- MyModule/src/main/java/com/autiguigu/HelloGit.java
+++ MyModule/src/main/java/com/autiguigu/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
     public static void main(String [] args){
         System.out.println("hello git");
         System.out.println("更新1");
+        System.out.println("更新2");
     }
 
 
